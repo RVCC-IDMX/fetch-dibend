456ca2bbcc6663983761824e0c13e3443bda0663
David DiBenedetto <david@njweb.solutions>
2022-12-10 18:40:36 -0500

    Final product
    Finished randomized blog

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-fetch('https://unsplash.it/600/400')
-  .then(res => res.blob())
-  .then(blob => {
-    let img = document.createElement('img');
-    img.src = URL.createObjectURL(blob);
-    document.querySelector('body').appendChild(img);
-  });
-
   const postSection = document.querySelector('#posts');
   const postTemplate = document.querySelector('#post-template');
 
@@ -20,13 +12,22 @@ fetch('https://unsplash.it/600/400')
         const title = post.title;
         const body = post.body;
 
-        const newPost = document.importNode(postTemplate.content, true);
-        const postTitle = newPost.querySelector('.post__title');
-        const postBody = newPost.querySelector('.post__body');
+        fetch('https://unsplash.it/300/200')
+        .then(res => res.blob())
+        .then(blob => {
+          const newPost = document.importNode(postTemplate.content, true);
+          const postTitle = newPost.querySelector('.post__title');
+          const postBody = newPost.querySelector('.post__body');
+          const postImg = newPost.querySelector('.post__img');
 
-        postTitle.innerText = title;
-        postBody.innerText = body;
-        postSection.appendChild(newPost);
+          postImg.src = URL.createObjectURL(blob);
+          postTitle.innerText = title;
+          postBody.innerText = body;
+          postSection.appendChild(newPost);
+        });
+
       }
-    })
+    });
+
   }
+  getData();
